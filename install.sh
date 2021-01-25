7baa923e1b1363a807f9624afb2072172cc09368
Jakub Dudek <jakub.a.dudek@gmail.com>
2021-01-25 16:37:56 +0100

    Add npm packages to install script

--- install.sh
+++ install.sh
@@ -5,26 +5,29 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew bundle --file ${BASEDIR}/.Brewfile
+brew bundle --file ${BASEDIR}/Brewfile
 
 # Update submodules
 git submodule update --init --remote
 
 # vim
-ln -s ${BASEDIR}/.vimrc ~/.vimrc
-ln -s ${BASEDIR}/.vim ~/.vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+ln -s ${BASEDIR}/vim ~/.vim
 
 # zsh
-ln -s ${BASEDIR}/.zshrc ~/.zshrc
-ln -s ${BASEDIR}/.oh-my-zsh ~/.oh-my-zsh
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+ln -s ${BASEDIR}/oh-my-zsh ~/.oh-my-zsh
 
 # git
-ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
-ln -s ${BASEDIR}/.gitignore ~/.gitignore 
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/gitignore ~/.gitignore 
 
 # tmux
-ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
+ln -s ${BASEDIR}/tmux.conf ~/.tmux.conf
 
 # Karabiner Elements
 mkdir -p ~/.config/karabiner
 ln -s ${BASEDIR}/karabiner.json ~/.config/karabiner/karabiner.json 
+
+# NPM global scripts
+npm i -g trash-cli pure-prompt
