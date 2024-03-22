a714e5dd56aee5187bf773e781a6c8a3a72711ff
Jakub Dudek <jakub.a.dudek@gmail.com>
2024-03-22 10:33:14 +0100

    nvim: isort

--- nvim/lua/configs/conform.lua
+++ nvim/lua/configs/conform.lua
@@ -16,7 +16,7 @@ return { -- Formatter
 		format_on_save = { timeout_ms = 500, lsp_fallback = true },
 		formatters_by_ft = {
 			lua = { "stylua" },
-			python = { "black" },
+			python = { "isort", "black" },
 			json = { "prettier" },
 			javascript = { "prettier" },
 		},
