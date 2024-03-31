f3fd3c9db161c7f7329691655827b6ccca7e0156
Jakub Dudek <jakub.a.dudek@gmail.com>
2024-03-31 23:02:35 +0200

    xcodebuild.nvim feature flag

--- nvim/lua/configs/xcode.lua
+++ nvim/lua/configs/xcode.lua
@@ -5,6 +5,7 @@ return {
 		"stevearc/oil.nvim",
 	},
 	config = function()
+		vim.env.XBS_FEAT_NEWFILE = 1
 		require("xcodebuild").setup({
 			vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" }),
 		})
