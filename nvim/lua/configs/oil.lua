423ed176401523221ec7110cf6e1be06e46e666f
Jakub Dudek <jakub.a.dudek@gmail.com>
2024-04-03 13:34:12 +0200

    nvim: oil bindings

--- nvim/lua/configs/oil.lua
+++ nvim/lua/configs/oil.lua
@@ -3,7 +3,19 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {},
 	config = function()
-		require("oil").setup()
+		require("oil").setup({
+			keymaps = {
+				["<CR>"] = "actions.select",
+				["<C-v>"] = "actions.select_vsplit",
+				["<C-x>"] = "actions.select_split",
+				["p"] = "actions.preview",
+				["-"] = "actions.parent",
+			},
+			view_options = {
+				show_hidden = true,
+			},
+			use_default_keymaps = false,
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end,
 }
