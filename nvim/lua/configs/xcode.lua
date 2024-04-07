b3fae5e2fd9c906e1964c3985f863ae53b8aedff
Jakub Dudek <jakub.a.dudek@gmail.com>
2024-04-07 19:05:46 +0200

    nvim: xcodebuild bindings

--- nvim/lua/configs/xcode.lua
+++ nvim/lua/configs/xcode.lua
@@ -7,7 +7,16 @@ return {
 	config = function()
 		vim.env.XBS_FEAT_NEWFILE = 1
 		require("xcodebuild").setup({
-			vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>", { desc = "Show Xcodebuild Actions" }),
+			test_explorer = {
+				auto_open = false,
+			},
+			logs = {
+				auto_open = false,
+			},
 		})
+
+		vim.keymap.set("n", "<leader>X", "<cmd>XcodebuildPicker<cr>")
+		vim.keymap.set("n", "<leader>xt", "<cmd>XcodebuildTestExplorerToggle<cr>")
+		vim.keymap.set("n", "<leader>xl", "<cmd>XcodebuildToggleLogs<cr>")
 	end,
 }
